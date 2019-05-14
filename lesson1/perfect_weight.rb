5cb656e3f47ef35871d127dc8ad13f9ef631bf3e
Slava <slavik.tkachenko@gmail.com>
2019-05-14 16:20:09 +0600

    Initial commit

---
+++ lesson1/perfect_weight.rb
@@ -0,0 +1,23 @@
+# Идеальный вес. Программа запрашивает у пользователя имя и рост и
+# выводит идеальный вес по формуле <рост> - 110, после
+# чего выводит результат пользователю на экран с обращением по имени.
+# Если идеальный вес получается отрицательным, то выводится строка
+# "Ваш вес уже оптимальный".
+
+WEIGHT_COEFICIENT = 110 #coeficient
+
+puts "Your name: "
+name = gets.chomp.to_s.capitalize
+
+puts "Your height in centimeters (integers only): "
+height = Integer(gets.chomp)
+
+perfect_weight = height - WEIGHT_COEFICIENT
+
+if perfect_weight < 0
+  puts "#{name}, your weight is already optimal"
+else
+  puts "#{name}, your perfect weight is #{perfect_weight} kg."
+end
+
+new_hash = Hash.new
