33411b348b7ea157421cbde287367dd93d95e47a
Slava <slavik.tkachenko@gmail.com>
2019-05-15 16:50:01 +0600

    commit lesson2

---
+++ lesson2/fib.rb
@@ -0,0 +1,14 @@
+# Заполнить массив числами фибоначчи до 100
+
+def fib(n)
+  fibo = [0, 1]
+  i = 0
+  while i < n
+    puts fibo[i]
+    fibo_next = fibo[i] + fibo[i.next]
+    fibo << fibo_next
+    i += 1
+  end
+end
+
+fib(100)
